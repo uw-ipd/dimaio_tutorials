3f95d1a346c100d71878a41360e872047d8a8393
Danny Farrell <danpf@uw.edu>
2020-05-19 16:36:04 -0700

    update bash

--- docs/_includes/custom/density_demo_files/5_rosettaES/B1_SampleSegment1.sh
+++ docs/_includes/custom/density_demo_files/5_rosettaES/B1_SampleSegment1.sh
@@ -6,6 +6,6 @@ python $ROSETTA3/source/scripts/python/public/EnumerativeSampling/RunRosettaES.p
     -f t20sA.fasta \
     -p input.pdb \
     -d T20S_48A_alpha_chainA.mrc \
-    -l 1\
-    -c 16\
-    -n loop_1\
+    -l 1 \
+    -c 16 \
+    -n loop_1
